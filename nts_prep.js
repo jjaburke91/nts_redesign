97451ea4dc696d702886efb7f74241b21cab0701
James Burke <jjaburke@hotmail.com>
2016-05-03 22:02:38 +0100

    changing between shows done.

--- nts_prep.js
+++ nts_prep.js
@@ -1,11 +1,17 @@
 
+
 /*** NTS Stream ***/
 
+var nts_stream_classes = {
+    visible_stream: 'stream-info__content--visible',
+    playing_stream: 'stream-info__content--playing'
+};
+
 var nts_streams = {};
-nts_streams.selected_stream = 'ldn';
 nts_streams.viewable = true;
 nts_streams.streams = {
     'ldn': {
+        streamName: 'ldn',
         title: "The Do!! You!! w/ Charlie Bones",
         tracklist: [
             'SILVER, HORACE-CHERRY BLOSSOM (RUDY VAN GELDER EDITION) (2009 DIGITAL REMASTER)',
@@ -29,6 +35,7 @@ nts_streams.streams = {
         ]
     },
     'la': {
+        streamName: 'la',
         title: "Kutmah",
         tracklist: [
             "EDAN-TIME OUT",
@@ -45,14 +52,24 @@ nts_streams.streams = {
         ]
     },
     'int': {
+        streamName: 'int',
         title: 'INT: Dolly Parton Gabba Special',
         tracklist: [
             'https://www.youtube.com/watch?v=dh5En5zb6PI'
         ]
     }
 };
+nts_streams.selected_stream = nts_streams.streams['ldn'];
+
 nts_streams.selectStream = function(streamName) {
-    nts_streams.selected_stream = nts_streams.streams[streamName] || false;
+    if ( nts_streams.streams[streamName] && (nts_streams.selected_stream != nts_streams.streams[streamName])) {
+        var oldVisibleStream = nts_streams.selected_stream;
+        nts_streams.selected_stream = nts_streams.streams[streamName] || false;
+        $("#" + oldVisibleStream.streamName + '-stream').removeClass(nts_stream_classes.visible_stream);
+        $("#" + nts_streams.selected_stream.streamName + '-stream').addClass(nts_stream_classes.visible_stream);
+    }
+
+    console.log("selecting '" + streamName + "'");
 };
 nts_streams.toggleViewable = function() {
     nts_streams.viewable = !nts_streams.viewable;
