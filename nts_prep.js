69ed6b781019a11fea1a60a687b67ae3b0f16500
James Burke <jjaburke@hotmail.com>
2016-05-04 20:05:29 +0100

    Code tidy.

--- nts_prep.js
+++ nts_prep.js
@@ -4,30 +4,21 @@
 var nts_stream_classes = {
     visible_stream: 'stream-info--visible',
     playing_stream: 'stream-info--playing',
-    nav_stream: 'nts-stream-cell--active',
-
-    // Animations
-    slide_to_left: 'stream-slideToLeft',
-    slide_to_right: 'stream-slideToRight',
-    slide_from_right: 'stream-slideFromRight',
-    slide_from_left: 'stream-slideLeftRight'
+    nav_stream: 'nts-stream-cell--active'
 };
 
 var nts_streams = {};
 nts_streams.streams = {
     'ldn': {
         streamName: 'ldn',
-        title: "The Do!! You!! w/ Charlie Bones",
         order: 1
     },
     'la': {
         streamName: 'la',
-        title: "Kutmah",
         order: 2
     },
     'int': {
         streamName: 'int',
-        title: 'INT: Dolly Parton Gabba Special',
         order: 3
     }
 };
@@ -53,23 +44,13 @@ nts_streams.selectStream = function(streamName) {
         var oldStreamElement = $("#" + oldVisibleStream.streamName + '-stream');
         var newStreamElement = $("#" + nts_streams.streams[streamName].streamName + "-stream");
 
-
         // Use these two classes for shrinking affect on change
         oldStreamElement.removeClass(nts_stream_classes.visible_stream);
         newStreamElement.addClass(nts_stream_classes.visible_stream);
-
-        if (oldVisibleStream.order < nts_streams.selected_stream.order) {
-            // oldVisibleStream.addClass( nts_streams.nav_stream);;
-        }
-
-        // slideStreams( oldStreamElement, newStreamElement);
-
-        console.log("selecting '" + streamName + "'");
     }
 
 };
 nts_streams.toggleViewable = function(viewableValue, streamName) {
-    console.log("setting viewable to " + (viewableValue || !nts_streams.viewable));
     nts_streams.viewable = viewableValue || !nts_streams.viewable;
 
     if (nts_streams.viewable) {
@@ -82,21 +63,7 @@ nts_streams.toggleViewable = function(viewableValue, streamName) {
     }
 };
 
-function slideStreams(oldElement, newElement) {
-
-}
-
-/*
-function changePlayingStream(oldStream, newStream) {
-    var oldStreamNavElement = $("#" + oldVisibleStream.streamName + '-nav');
-    var newStreamNavElement = $("#" + nts_streams.streams[streamName].streamName + "-nav");
-    oldStreamNavElement.removeClass(nts_stream_classes.nav_stream);
-    newStreamNavElement.addClass(nts_stream_classes.nav_stream);
-}
-*/
-
 /*** On Page Load ***/
-
 $(document).ready(function() {
     var startingStream = 'la'
     nts_streams.selectStream( startingStream);
